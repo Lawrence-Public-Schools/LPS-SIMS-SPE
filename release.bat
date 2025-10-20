c5081f0da1406e0c6519148c28e1c913c5bc65c3
Logan Arias <71857092+Logan-Arias-LPS@users.noreply.github.com>
2025-10-20 13:24:29 -0400

    rename leftover parts of ELL to SPE
    rename leftover parts of ELL to SPE

--- release.bat
+++ release.bat
@@ -1,4 +1,4 @@
 @echo off
 mkdir dists
-git archive --format zip --output dists\LPS-SIMS-ELL.zip --worktree-attributes --verbose -9 HEAD
+git archive --format zip --output dists\LPS-SIMS-SPE.zip --worktree-attributes --verbose -9 HEAD
 pause
